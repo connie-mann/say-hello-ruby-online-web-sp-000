8db02d252ba98f4fb8770482adedf319e15a9b10
Constance Mann <connie.mann@yahoo.com>
2021-01-25 20:03:42 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -18,7 +18,3 @@ def say_hello (name = "Ruby Programming")
 end
 
 say_hello ("Gabriela")
-
-def greeting(name="Ruby programmer", language="Ruby")
-  puts "Hello, #{name}. Hello #{language} programmer."
-end
